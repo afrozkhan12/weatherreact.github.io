d73fd8b62f5856cf4b052740ecde92d17860940a
Asim khan <122780482+afrozkhan12@users.noreply.github.com>
2023-05-26 09:39:40 +0530

    done 2

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import Todo from "./todoreact/todo";
+import Temp from "./weather/temp";
+
+const App = () => {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+};
+
+export default App;
